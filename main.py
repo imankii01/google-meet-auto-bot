a09c248744ef82ad4a0b8d565a9510aa20c7e2c6
Ankit <private.ankit047@gmail.com>
2024-12-06 18:01:02 +0530

    headless

--- main.py
+++ main.py
@@ -67,9 +67,9 @@ class JoinGoogleMeetAsGuest:
 
         options = Options()
         
+        options.add_argument("--headless")
         options.add_argument("--disable-blink-features=AutomationControlled")
         options.add_argument("--start-maximized")
-        # options.add_argument("--headless=new")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--disable-gpu")
